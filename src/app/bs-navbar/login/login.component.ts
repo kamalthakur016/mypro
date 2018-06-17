4f05108e9ea79820089ea8fe011f6fad3ee2a172
unknown <kamalec017@gmail.com>
2018-06-17 18:19:00 +0530

    sign in with permission

--- src/app/bs-navbar/login/login.component.ts
+++ src/app/bs-navbar/login/login.component.ts
@@ -1,16 +1,22 @@
+import { BsNavbarComponent } from './../bs-navbar.component';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Component } from '@angular/core';
 import * as firebase from 'firebase';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent  {
-
-  constructor(private afAuth: AngularFireAuth) { }
+  user = this.user;
+  constructor(private afAuth: AngularFireAuth) {
+  
+   }
 
   login(){
-    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+
+     this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  
   }
 }
